460f287975d71d1554812292f1f23d5ed1be1acb
MD AL EMRAN <mdalemrananas@gmail.com>
2025-09-16 23:38:41 +0600

    Lists in Python

---
+++ Lists/Exercise5.py
@@ -0,0 +1,6 @@
+''' Exercise 5: Find the length, sum, and check if 15 is in [5, 10, 15, 20]'''
+
+lst = [5, 10, 15, 20]
+print("Length:", len(lst))
+print("Sum:", sum(lst))
+print("Is 15 present?", 15 in lst)
